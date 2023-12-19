fac1b4d91003398b867b8b4845b5713df4b0c26e
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2023-12-19 15:17:11 +0530

    upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tab){
+   for(tablink of tablinks){
+    tablink.classList.remove("active-link");
+   }
+   for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+   }
+
+   event.currentTarget.classList.add("active-link");
+   document.getElementById(tab).classList.add("active-tab");
+}
+
+
+let ul = document.querySelector("#sidemenu");
+function showmenu(){
+  ul.style.right = "0px";
+}
+
+
+function hidemenu(){
+    ul.style.right = "-200px";
+  }
